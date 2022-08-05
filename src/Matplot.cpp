61bf5748d2f4df2e069a8925297d6bff3374c625
uhobeike <beike315@icloud.com>
2022-08-05 11:27:04 +0000

    Update matplot

--- src/Matplot.cpp
+++ src/Matplot.cpp
@@ -5,16 +5,17 @@ namespace plt = matplotlibcpp;
 namespace emcl2 {
     void Matplot::particle_usescan_angle_plot(std::vector<int> particle_num, std::vector<int> scan_angle)
     {
-        plt::plot(particle_num, scan_angle, "|");
+        plt::plot(particle_num, scan_angle, "k|");
     }
     
     void Matplot::particle_usescan_angle_plot(std::vector<double> particle_num, std::vector<double> scan_angle)
     {
-        plt::plot(particle_num, scan_angle, "|");
+        plt::plot(particle_num, scan_angle, "k|");
     }
 
     void Matplot::show()
     {
+        plt::ylim(0.0, 1.0);
         plt::legend();
         // plt::show();
         plt::pause(0.01);
