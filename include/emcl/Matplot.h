c17cdc6249c3fa1c9c1a21e696e104e87fa93fa9
uhobeike <beike315@icloud.com>
2022-09-11 04:13:48 +0000

    Update

--- include/emcl/Matplot.h
+++ include/emcl/Matplot.h
@@ -1,5 +1,5 @@
-//SPDX-FileCopyrightText: 2022 Ryuichi Ueda ryuichiueda@gmail.com
-//SPDX-License-Identifier: LGPL-3.0-or-later
+// SPDX-FileCopyrightText: 2022 Ryuichi Ueda ryuichiueda@gmail.com
+// SPDX-License-Identifier: LGPL-3.0-or-later
 
 #ifndef MATPLOT_H__
 #define MATPLOT_H__
@@ -10,18 +10,23 @@ namespace plt = matplotlibcpp;
 
 namespace emcl2 {
 
-class Matplot
-{
+class Matplot {
 public:
-	Matplot(){}
+  Matplot() {}
 
-    void particle_usescan_angle_plot(std::vector<int> particle_num, std::vector<int> scan_angle);
-    void particle_usescan_angle_plot(std::vector<double> particle_num, std::vector<double> scan_angle);
-    void show();
-    void clear();
+  void particle_usescan_angle_plot(std::vector<int> particle_num,
+                                   std::vector<int> scan_angle);
+  void particle_usescan_angle_plot(std::vector<double> particle_num,
+                                   std::vector<double> scan_angle);
+  void particle_weight_plot(std::vector<double> particle_num,
+                            std::vector<double> particle_weight);
 
+  void show();
+  void clear();
+  void show_weight();
+  void clear_weight();
 };
 
-}
+} // namespace emcl2
 
 #endif
